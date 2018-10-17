ae26b708b923902b0a6f0b1af1b96ec89d5db8e9
Billy Witherspoon <wwither@gmail.com>
2018-10-17 02:08:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-var greeting = "";
+var greeting = "Hello, everybody!";
 
-var specialGuest = "Neil deGrasse Tyson"
+var specialGuest = "Neil deGrasse Tyson";
 
 var greetSpecialGuest = "" + specialGuest + "!";
 
-var topic = "space";
+var topic = "Let\'s talk about Let\'s talk about space";
 
 var conversation = `${topic}`;
+console.log("est");
